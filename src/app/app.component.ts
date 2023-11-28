bc8a5fb72783e8d7ee73f2eac566bb040ad45cd4
kumarpankaj2315 <74148451+kumarpankaj2315@users.noreply.github.com>
2023-11-28 15:38:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'form-app';
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = fb.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mobile: new FormControl(0, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      address: new FormControl('', [Validators.required]),
+      gender: new FormControl(null, [Validators.required]),
+      consent: new FormControl(false, [Validators.required]),
+    });
+  }
+
+  get userFormControl() {
+    return this.userForm.controls;
+  }
+
+  submitForm() {
+    console.log(this.userForm.value, this.userForm.valid);
+  }
 }
