0bc3407dcfcf2d4db0ccebd0b9a6694346bead04
kumarpankaj2315 <74148451+kumarpankaj2315@users.noreply.github.com>
2026-02-08 17:10:22 +0530

    added user details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   FormControl,
   Validators,
 } from '@angular/forms';
+import {
+  Subject,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-app';
   userForm: FormGroup;
+  userName: string = 'Pankaj Kumar';
+  userData: string = '';
+  userEnterEvent = new Subject();
+  temperature: Number = 0;
+  selectedType = new Subject();
+  tempreatureSubject = new Subject();
 
   constructor(private fb: FormBuilder) {
     this.userForm = fb.group({
@@ -21,7 +33,7 @@ export class AppComponent {
       email: new FormControl('', [Validators.required, Validators.email]),
       mobile: new FormControl(0, [
         Validators.required,
-        Validators.minLength(10),
+        Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
       ]),
       address: new FormControl('', [Validators.required]),
       gender: new FormControl(null, [Validators.required]),
@@ -33,7 +45,32 @@ export class AppComponent {
     return this.userForm.controls;
   }
 
+  ngOnInit(): void {
+    this.getUserInputData();
+    this.getTemp();
+  }
+
   submitForm() {
     console.log(this.userForm.value, this.userForm.valid);
+  }
+
+  getUserInputData() {
+    this.userEnterEvent
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((data) => {
+        console.log('data', data);
+      });
+  }
+
+  onOptionsSelected(value: string) {
+    console.log(value);
+    console.log('check something');
+    this.selectedType.next(value);
+  }
+
+  getTemp() {
+    combineLatest([this.tempreatureSubject, this.selectedType]).subscribe({
+      next: ([temp, selectVal]) => console.log(temp, selectVal),
+    });
   }
 }
